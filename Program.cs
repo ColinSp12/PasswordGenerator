4b2b4a4d8d4e97c4e4969d9c71c3775bfde86aaa
Colin Spetz <colin.spetz@Student.SL.On.Ca>
2023-01-25 15:26:13 -0500

    Attempting outputting to a file

--- Program.cs
+++ Program.cs
@@ -11,35 +11,29 @@ class RandomPassword
         var watch = new System.Diagnostics.Stopwatch();
         watch.Start();
 
-        for (int ctr = 0; ctr < 1000; ctr++)
+        using (FileStream target = File.Create(@"c:\users\colin\source\repos\COMP 60\Week3Practice\data.txt"))
         {
-
-            var bytes = new byte[16];
-            using (var rng = RandomNumberGenerator.Create())
+            using (StreamWriter writer = new StreamWriter(target))
             {
-                rng.GetBytes(bytes);
+                for (int ctr = 0; ctr < 1000; ctr++)
+                {
+
+                    var bytes = new byte[16];
+                    using (var rng = RandomNumberGenerator.Create())
+                    {
+                        rng.GetBytes(bytes);
+                    }
+                    var password = string.Concat(bytes.Select(b => b.ToString("X2")));
+                    Console.WriteLine(password.Substring(0, 16) + " : " + ctr);
+
+                }
             }
-            var password = string.Concat(bytes.Select(b => b.ToString("X2")));
-            Console.WriteLine(password.Substring(0, 16)  + " : " + ctr);
-            
         }
         watch.Stop();
         Console.WriteLine($"Execution Time: {watch.Elapsed.TotalMilliseconds} ms");
         Console.WriteLine($"Execution Time: {watch.Elapsed.TotalSeconds} secs");
         Console.WriteLine($"Execution Time: { watch.ElapsedTicks} ticks");
 
-        WriteToAFileWithFileStream();
-
         Console.ReadLine();
-    }
-    static private void WriteToAFileWithFileStream()
-    {
-        using (FileStream target = File.Create(@"c:\mydirectory\target.txt"))
-        {
-            using (StreamWriter writer = new StreamWriter(target))
-            {
-                writer.WriteLine("Hello world");
-            }
-        }
     }
 }
