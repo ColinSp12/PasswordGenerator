24e0acc76855fdd566fc42e0ce1540e88eed4d4e
Colin Spetz <colin.spetz@Student.SL.On.Ca>
2023-01-25 13:02:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+class RandomPassword
+{
+    public static void Main(string[] args)
+    {
+        for (int ctr = 0; ctr < 100; ctr++)
+        {
+            var bytes = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            var password = string.Concat(bytes.Select(b => b.ToString("X2")));
+            Console.WriteLine(password.Substring(0, 16)  + " : " + ctr);
+        }
+        Console.ReadLine();
+    }
+}
