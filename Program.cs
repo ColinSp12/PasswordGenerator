7b1f45284c3bfa743a8fffff566f46de825d8ea4
Colin Spetz <colin.spetz@Student.SL.On.Ca>
2023-01-25 13:28:23 -0500

    Generates random 16 bit password

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Linq;
 using System.Security.Cryptography;
-
+using System.IO;
+//Program that generates a random 16 bit password
+//Colin Spetz
 class RandomPassword
 {
     public static void Main(string[] args)
     {
-        for (int ctr = 0; ctr < 100; ctr++)
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+
+        for (int ctr = 0; ctr < 1000; ctr++)
         {
+
             var bytes = new byte[16];
             using (var rng = RandomNumberGenerator.Create())
             {
@@ -15,7 +21,25 @@ class RandomPassword
             }
             var password = string.Concat(bytes.Select(b => b.ToString("X2")));
             Console.WriteLine(password.Substring(0, 16)  + " : " + ctr);
+            
         }
+        watch.Stop();
+        Console.WriteLine($"Execution Time: {watch.Elapsed.TotalMilliseconds} ms");
+        Console.WriteLine($"Execution Time: {watch.Elapsed.TotalSeconds} secs");
+        Console.WriteLine($"Execution Time: { watch.ElapsedTicks} ticks");
+
+        WriteToAFileWithFileStream();
+
         Console.ReadLine();
+    }
+    static private void WriteToAFileWithFileStream()
+    {
+        using (FileStream target = File.Create(@"c:\mydirectory\target.txt"))
+        {
+            using (StreamWriter writer = new StreamWriter(target))
+            {
+                writer.WriteLine("Hello world");
+            }
+        }
     }
 }
